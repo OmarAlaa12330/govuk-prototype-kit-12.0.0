763881dce0dd364ac700cd7e0ca3f70181b94f3d
OmarAlaa12330 <omar.alaa12330@gmail.com>
2022-01-17 16:27:52 +0000

    second change

---
+++ app/views/Some-Js/ContactJs.js
@@ -0,0 +1,24 @@
+/*var profile = [
+  {
+    "img":" ",
+    "name": {
+      "nameTxt":"Omar Hussein",
+      "nameURL":"https://intranet.dft.gov.uk/users/omarhussaingovuk"
+    },
+    "role": {
+      "roleTxt":"Associate Business Analyst",
+      "roleURL": ""
+    }
+  }
+];
+document.body.innerHTML=profile.map((data)=>{
+  return(
+    <section style="display:flex; flex-direction:column">
+      <img src ={data.img} alt= "profile image" />
+      <a href ={data.name.nameURL}>{data.name.nameTxt}</a>
+      <a href = {data.role.roleURL}>{data.role.roleTxt}</a>
+    </section>
+  )
+})
+*/
+document.body.innerHTML="test"
